ce26c7eab78932e4951eb553b464f977adad0d5c
andriyanovme <mathewsandrianoff@yandex.ru>
2023-06-09 21:10:33 +0300

    В ViewController добавил счетчик и кнопку

--- Counter2.0/ViewController.swift
+++ Counter2.0/ViewController.swift
@@ -1,19 +1,24 @@
 //
 //  ViewController.swift
 //  Counter2.0
-//
-//  Created by E on 09.06.2023.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var score: Int = 0
+    @IBOutlet weak var changeButton: UIButton!
+    @IBOutlet weak var countScoreLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func buttonDidTap(_ sender: Any) {
+        score += 1
+        countScoreLabel.text = "\(score)"
+    }
+    
 }
 
